4a77381d4f3af9e83d97a33dc4a0c363160aef43
Thore <40232329+ThoreBor@users.noreply.github.com>
2020-05-22 23:57:57 +0200

    new api
    thanks to  u/SmallFluffyIPA

--- server/views.py
+++ server/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import sqlite3
+import json
 
 def homepage(request):
 	return HttpResponse('<h3>More info <a href="https://ankiweb.net/shared/info/41708974">here.</a></h3>')
@@ -46,6 +47,38 @@ def sync(request):
 	return HttpResponse("Done!")
 
 @csrf_exempt
+def all_users(request):
+	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
+	c = conn.cursor()
+	Username_List = []
+	c.execute("SELECT Username FROM Leaderboard")
+	for i in c.fetchall():
+		data = str(i)
+		clean = ["(",")","'",","]
+		for j in clean:
+			data = data.replace(j, "")
+		Username_List.append(data)
+	return HttpResponse(json.dumps(Username_List))
+
+@csrf_exempt
+def get_data(request):
+	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
+	c = conn.cursor()
+	c.execute("SELECT * FROM Leaderboard ORDER BY Cards DESC")
+	data = []
+	for row in c.fetchall():
+		row = str(row)
+		clean = ["(",")","'","[","]", " "]
+		for i in clean:
+			row = row.replace(i, "")
+		data.append(row)
+	return HttpResponse(json.dumps(data))
+
+
+### OLD API ###
+
+
+@csrf_exempt
 def users(request):
 	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
 	c = conn.cursor()
@@ -58,23 +91,6 @@ def users(request):
 		    data = data.replace(j, "")
 		Username_List.append(data)
 	return HttpResponse(str(Username_List))
-
-@csrf_exempt
-def allusers(request):
-	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
-	c = conn.cursor()
-	Username_List = []
-	c.execute("SELECT Username FROM Leaderboard")
-	for i in c.fetchall():
-		data = str(i)
-		clean = ["(",")","'",",", "\\"]
-		for j in clean:
-		    data = data.replace(j, "")
-		Username_List.append(data)
-	data = ""
-	for i in Username_List:
-	    data = data + str(i) + "<br>"
-	return HttpResponse(str(data))
 
 @csrf_exempt
 def getreviews(request):
@@ -102,9 +118,6 @@ def delete(request):
     except:
         return HttpResponse("Failed")
 
-###########################################################
-###Only used for <v1.4###
-
 @csrf_exempt
 def getstreaks(request):
 	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
@@ -125,7 +138,7 @@ def gettime(request):
 	c.execute("SELECT * FROM Leaderboard ORDER BY Time_Spend DESC")
 	data = ""
 	for row in c.fetchall():
-	        data = data + str(row) + "<br>"
+		data = data + str(row) + "<br>"
 	clean = ["(",")","'","[","]", " "]
 	for i in clean:
 	    data = data.replace(i, "")
