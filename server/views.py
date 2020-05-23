6bbdcd293b872e3141ce5e09457756fc85df35d3
Thore <40232329+ThoreBor@users.noreply.github.com>
2020-05-23 19:33:28 +0200

    add leaderboard website

--- server/views.py
+++ server/views.py
@@ -1,11 +1,85 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import sqlite3
 import json
+from datetime import datetime, timedelta
 
-def homepage(request):
-	return HttpResponse('<h3>More info <a href="https://ankiweb.net/shared/info/41708974">here.</a></h3>')
+def reviews(request):
+	data = []
+	start_day = datetime.now() - timedelta(days=1)
+	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
+	c = conn.cursor()
+	c.execute("SELECT Username, Cards, Sync_Date FROM Leaderboard ORDER BY Cards DESC")
 
+	for row in c.fetchall():
+		sync_date = row[2]
+		sync_date = sync_date.replace(" ", "")
+		if len(sync_date) == 10:
+			sync_date = sync_date + "12:00:00"
+		sync_date = datetime(int(sync_date[0:4]),int(sync_date[5:7]), int(sync_date[8:10]), int(sync_date[10:12]), int(sync_date[13:15]), int(sync_date[16:18]))
+
+		if sync_date > start_day:
+			x = {"username": row[0], "value": row[1]}
+			data.append(x)
+	return render(request, "reviews.html", {"data": data})
+
+def time(request):
+	data = []
+	start_day = datetime.now() - timedelta(days=1)
+	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
+	c = conn.cursor()
+	c.execute("SELECT Username, Time_Spend, Sync_Date FROM Leaderboard ORDER BY Time_Spend DESC")
+
+	for row in c.fetchall():
+		sync_date = row[2]
+		sync_date = sync_date.replace(" ", "")
+		if len(sync_date) == 10:
+			sync_date = sync_date + "12:00:00"
+		sync_date = datetime(int(sync_date[0:4]),int(sync_date[5:7]), int(sync_date[8:10]), int(sync_date[10:12]), int(sync_date[13:15]), int(sync_date[16:18]))
+
+		if sync_date > start_day:
+			x = {"username": row[0], "value": row[1]}
+			data.append(x)
+	return render(request, "time.html", {"data": data})
+
+def streak(request):
+	data = []
+	start_day = datetime.now() - timedelta(days=1)
+	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
+	c = conn.cursor()
+	c.execute("SELECT Username, Streak, Sync_Date FROM Leaderboard ORDER BY Streak DESC")
+
+	for row in c.fetchall():
+		sync_date = row[2]
+		sync_date = sync_date.replace(" ", "")
+		if len(sync_date) == 10:
+			sync_date = sync_date + "12:00:00"
+		sync_date = datetime(int(sync_date[0:4]),int(sync_date[5:7]), int(sync_date[8:10]), int(sync_date[10:12]), int(sync_date[13:15]), int(sync_date[16:18]))
+
+		if sync_date > start_day:
+			x = {"username": row[0], "value": row[1]}
+			data.append(x)
+	return render(request, "streak.html", {"data": data})
+
+def retention(request):
+	data = []
+	start_day = datetime.now() - timedelta(days=1)
+	conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
+	c = conn.cursor()
+	c.execute("SELECT Username, Retention, Sync_Date FROM Leaderboard ORDER BY Retention DESC")
+
+	for row in c.fetchall():
+		sync_date = row[2]
+		sync_date = sync_date.replace(" ", "")
+		if len(sync_date) == 10:
+			sync_date = sync_date + "12:00:00"
+		sync_date = datetime(int(sync_date[0:4]),int(sync_date[5:7]), int(sync_date[8:10]), int(sync_date[10:12]), int(sync_date[13:15]), int(sync_date[16:18]))
+
+		if sync_date > start_day and row[1] != "N/A" and row[1] != "":
+			x = {"username": row[0], "value": row[1]}
+			data.append(x)
+	return render(request, "retention.html", {"data": data})
 
 @csrf_exempt
 def sync(request):
@@ -138,7 +212,7 @@ def gettime(request):
 	c.execute("SELECT * FROM Leaderboard ORDER BY Time_Spend DESC")
 	data = ""
 	for row in c.fetchall():
-		data = data + str(row) + "<br>"
+	        data = data + str(row) + "<br>"
 	clean = ["(",")","'","[","]", " "]
 	for i in clean:
 	    data = data.replace(i, "")
