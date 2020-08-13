1f6be15ef24040c402c18f040e8f9727b2ff3065
Thore <40232329+ThoreBor@users.noreply.github.com>
2020-08-13 16:28:50 +0200

    simplify code

--- server/manage_leagues.py
+++ server/manage_leagues.py
@@ -1,18 +1,9 @@
 import sqlite3
 
 alpha_ranking = []
-alpha_bottom = []
-
 beta_ranking = []
-beta_top = []
-beta_bottom = []
-
 gamma_ranking = []
-gamma_top = []
-gamma_bottom = []
-
 delta_ranking = []
-delta_top = []
 
 #conn = sqlite3.connect('/home/ankileaderboard/anki_leaderboard_pythonanywhere/Leaderboard.db')
 conn = sqlite3.connect('Leaderboard.db')
@@ -34,33 +25,22 @@ for row in c.fetchall():
 
 print(f"Alpha: {len(alpha_ranking)} \nBeta: {len(beta_ranking)} \nGamma: {len(gamma_ranking)} \nDelta: {len(delta_ranking)}")
 
-alpha_bottom = alpha_ranking[-int((len(alpha_ranking) / 100) * 20):]
-for i in alpha_bottom:
+for i in alpha_ranking[-int((len(alpha_ranking) / 100) * 20):]:
 	c.execute("UPDATE League SET league = (?) WHERE username = (?) ", ("Beta", i))
-conn.commit()
 
-beta_top = beta_ranking[:int((len(beta_ranking) / 100) * 20)]
-for i in beta_top:
+for i in beta_ranking[:int((len(beta_ranking) / 100) * 20)]:
 	c.execute("UPDATE League SET league = (?) WHERE username = (?) ", ("Alpha", i))
-conn.commit()
-beta_bottom = beta_ranking[-int((len(beta_ranking) / 100) * 20):]
-for i in beta_bottom:
+for i in beta_ranking[-int((len(beta_ranking) / 100) * 20):]:
 	c.execute("UPDATE League SET league = (?) WHERE username = (?) ", ("Gamma", i))
-conn.commit()
-
-gamma_top = gamma_ranking[:int((len(gamma_ranking) / 100) * 20)]
-for i in gamma_top:
+ 
+for i in gamma_ranking[:int((len(gamma_ranking) / 100) * 20)]:
 	c.execute("UPDATE League SET league = (?) WHERE username = (?) ", ("Beta", i))
-conn.commit()
-gamma_bottom = gamma_ranking[-int((len(gamma_ranking) / 100) * 20):]
-for i in gamma_bottom:
+for i in gamma_ranking[-int((len(gamma_ranking) / 100) * 20):]:
 	c.execute("UPDATE League SET league = (?) WHERE username = (?) ", ("Delta", i))
-conn.commit()
 
-delta_top = delta_ranking[:int((len(delta_ranking) / 100) * 20)]
-for i in delta_top:
+for i in delta_ranking[:int((len(delta_ranking) / 100) * 20)]:
 	c.execute("UPDATE League SET league = (?) WHERE username = (?) ", ("Gamma", i))
-conn.commit()
 
 c.execute("UPDATE League SET xp = 0, time_spend = 0, reviews = 0, retention = 0")
+
 conn.commit()
