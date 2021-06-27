d7c1a7c4df4ec777a24ae5eac626b8a38bc0e10b
Thore <thore.tyborski@gmail.com>
2021-06-27 19:45:02 +0200

    fix league medals bug - again

--- League.py
+++ League.py
@@ -36,11 +36,11 @@ def load_league(self, colors):
 			days_learned = "n/a"
 
 		if i[6]:
-			history = json.loads(i[6])
-			if history["gold"] != 0 or history["silver"] != 0 or history["bronze"] != 0:
-				medal_users.append([username, history["gold"], history["silver"], history["bronze"]])
 			if config["show_medals"] == True:
-				username = f"{username} |"
+				history = json.loads(i[6])
+				if history["gold"] != 0 or history["silver"] != 0 or history["bronze"] != 0:
+					medal_users.append([username, history["gold"], history["silver"], history["bronze"]])
+					username = f"{username} |"
 				if history["gold"] > 0:
 					username = f"{username} {history['gold'] if history['gold'] != 1 else ''}🥇"
 				if history["silver"] > 0:
