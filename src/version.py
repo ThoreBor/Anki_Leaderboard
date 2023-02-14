054c44ed5fd36953b0eeb81600c7a01102b0fdd5
Thore <thore.tyborski@gmail.com>
2023-02-14 17:01:27 +0100

    update about text

--- src/version.py
+++ src/version.py
@@ -2,37 +2,49 @@ version = "v3.0.2"
 
 about_text = f"""
 <h2>Leaderboard {version}</h2>
-This add-on ranks all of its users by the number of cards reviewed today, time spend studying today, 
-current streak, reviews in the past 31 days, and retention. You can also compete against friends, join groups, 
-and join a country leaderboard. You'll only see users, that synced on the same day as you.<br><br>
-In the league tab, you see everyone who synced at least once during the current season. There are four leagues
-(Alpha, Beta, Gamma, and Delta). A season lasts two weeks. You don't have to sync every day.
-The XP formula is:<br><b>XP = days studied percentage x ((6 x time) + (2 x reviews x retention))</b>.<br>
-You have to study at least 5 minutes per day. Otherwise, this day won't be counted as “studied”
-(<i><a href="https://github.com/ThoreBor/Anki_Leaderboard/issues/122">See this issue for more info<a/></i>).
-At the end of each season, the top 20% will be promoted, and the last 20% will be relegated.<br><br>
-The code for the add-on is available on <a href="https://github.com/ThoreBor/Anki_Leaderboard">GitHub.</a> 
-It is licensed under the <a href="https://github.com/ThoreBor/Anki_Leaderboard/blob/master/LICENSE">MIT License.</a> The privacy policy can
-found <a href="https://ankileaderboard.pythonanywhere.com/privacy/">here</a>.
-If you like this add-on, rate and review it on <a href="https://ankiweb.net/shared/info/41708974">AnkiWeb.</a><br><br>
-You can also check the leaderboard (past 24 hours) on this <a href="https://ankileaderboard.pythonanywhere.com/">website</a>.<br>
-<div>Crown icon made by <a href="https://www.flaticon.com/de/autoren/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/de/" title="Flaticon">www.flaticon.com</a></div>
-<div>Person icon made by <a href="https://www.flaticon.com/de/autoren/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/de/" title="Flaticon">www.flaticon.com</a></div>
-<div>Settings icon made by <a href="https://www.flaticon.com/free-icons/setting" title="setting icons">Setting icons created by Phoenix Group - Flaticon</a>
-<div>Confetti gif from <a href="https://giphy.com/stickers/giphycam-rainbow-WNJATm9pwnjpjI1i0g">Giphy</a></div><br>
-Contact: leaderboard_support@protonmail.com, <a href="https://www.reddit.com/user/Ttime5">Reddit</a> or 
-<a href="https://github.com/ThoreBor/Anki_Leaderboard">GitHub</a>.
-<br><br>
-<b>© Thore Tyborski 2023<br>
-With contributions from <a href="https://github.com/khonkhortisan">khonkhortisan</a>, 
-<a href="https://github.com/zjosua">zjosua</a>, 
-<a href="https://www.reddit.com/user/SmallFluffyIPA/">SmallFluffyIPA</a>, 
-<a href="https://github.com/AtilioA">Atílio Antônio Dadalto</a> and
-<a href="https://github.com/rodrigolanes">Rodrigo Lanes</a><br>
-Also thank you to everyone who reported bugs and suggested new features!</b>
+<p>
+	This add-on ranks all of its users by the number of cards reviewed today, time spend studying today, current streak, reviews in the past 31 days, 
+	and retention. You can also compete against friends, join groups, and join a country leaderboard. You'll only see users, that synced on the same day as you. 
+	In the league tab, you see everyone who synced at least once during the current season. There are four leagues (Alpha, Beta, Gamma, and Delta). 
+	A season lasts two weeks. You don't have to sync every day. The XP formula is:
+	<br><b>XP = days studied percentage x ((6 x time) + (2 x reviews x retention))</b>.<br>
+	You have to study at least 5 minutes per day. Otherwise, this day won't be counted as “studied” 
+	(<i><a href="https://github.com/ThoreBor/Anki_Leaderboard/issues/122">See this issue for more info<a/></i>). 
+	At the end of each season, the top 20% will be promoted, and the last 20% will be relegated.
+</p>
+<p>
+	The code for the add-on is available on <a href="https://github.com/ThoreBor/Anki_Leaderboard">GitHub.</a> 
+	It is licensed under the <a href="https://github.com/ThoreBor/Anki_Leaderboard/blob/master/LICENSE">MIT License.</a> The privacy policy can
+	found <a href="https://ankileaderboard.pythonanywhere.com/privacy/">here</a>.
+	If you like this add-on, rate and review it on <a href="https://ankiweb.net/shared/info/41708974">AnkiWeb.</a>
+	You can also check the leaderboard (past 24 hours) on this <a href="https://ankileaderboard.pythonanywhere.com/">website</a>.
+</p>
+	
+<h3>This add-on includes the following third-party assets and code</h3>
+<p>
+	Crown icon made by <a href="https://www.flaticon.com/de/autoren/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/de/" title="Flaticon">www.flaticon.com</a><br>
+	Person icon made by <a href="https://www.flaticon.com/de/autoren/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/de/" title="Flaticon">www.flaticon.com</a><br>
+	Settings icon made by <a href="https://www.flaticon.com/free-icons/setting" title="setting icons">Setting icons created by Phoenix Group - Flaticon</a><br>
+	<a href="https://github.com/catdad/canvas-confetti">canvas-confetti</a> by <a href="https://github.com/catdad">Kiril Vatev</a> licensed under the <a href="https://github.com/catdad/canvas-confetti/blob/master/LICENSE">ISC License</a><br>
+	<a href="https://codepen.io/nikitahl/pen/xmvVmW">Tear off calendar</a> by <a href="https://codepen.io/nikitahl">Nikita Hlopov</a>
+</p>
+
+
 <h3>Change Log:</h3>
 - fixed bug when trying to open the leaderboard without being logged in<br>
-- fixed highlight bug<br><br>
+- fixed highlight bug<br>
+You can check out the full change log on <a href="https://github.com/ThoreBor/Anki_Leaderboard/blob/master/changelog.md">GitHub</a>.
 
-<b>You can check out the full change log on <a href="https://github.com/ThoreBor/Anki_Leaderboard/blob/master/changelog.md">GitHub</a>.</b>
+<p>
+	<b>© Thore Tyborski 2023</b><br>
+	With contributions from 
+	<a href="https://github.com/khonkhortisan">khonkhortisan</a>, 
+	<a href="https://github.com/zjosua">zjosua</a>, 
+	<a href="https://www.reddit.com/user/SmallFluffyIPA/">SmallFluffyIPA</a>, 
+	<a href="https://github.com/AtilioA">Atílio Antônio Dadalto</a> and
+	<a href="https://github.com/rodrigolanes">Rodrigo Lanes</a><br>
+	Also thank you to everyone who reported bugs and suggested new features!<br><br>
+	Contact: leaderboard_support@protonmail.com, <a href="https://www.reddit.com/user/Ttime5">Reddit</a> or 
+	<a href="https://github.com/ThoreBor/Anki_Leaderboard">GitHub</a>.
+</p>
 """
